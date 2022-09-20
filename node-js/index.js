40b8ffbf01ead194d64ca743556e78f57e538733
sfjbssept <sfjbssept@gmail.com>
2022-09-20 15:23:41 +0000

    json response with 200 status code

--- node-js/index.js
+++ node-js/index.js
@@ -1,7 +1,16 @@
 console.log("hello World!")
 const http = require("http");
+const { json } = require("stream/consumers");
 http.createServer((req,res)=>{
-    res.write("Server is running");
-    // res.statusCode(200);
+    res.writeHead(200,{'content-type':'application/json'});
+    res.write(JSON.stringify({
+        "name":"rajeev",
+        "age":"30",
+        "address":{
+            "city":"mumbai",
+            "state":"maharastra",
+            "zip":"400066"
+        }
+    }));
     res.end();
 }).listen(5000)
