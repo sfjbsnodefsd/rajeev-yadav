133025922f2eb6ca0b094dd34a09b53be8c07d7a
sfjbssept <sfjbssept@gmail.com>
2022-09-26 14:41:08 +0000

    UPDATE, DELETE, MULTI INSERT - node_mongo_rest_API

--- node-js/rest_api_node_mongo/routes/course.js
+++ node-js/rest_api_node_mongo/routes/course.js
@@ -3,23 +3,85 @@ const Course = require("../models/course")
 
 courseRoute = express.Router();
 
+//get all course
 courseRoute.get("/course", async(req,res) => {
     try {
         const data = await Course.find({})
-        res.status(200).send(data)
+        if(data){
+            res.status(200).send(data)
+        }else{
+            res.status(200).send({"msg":"No Record found"});
+        }
     } catch (error) {
         res.status(500).send(error)
     }
 })
+//create course
 courseRoute.post("/course", async(req,res)=>{
     try {
         const course = new Course(req.body);
         const data = await course.save();
-        // console.log(data);
-        res.status(200).send("Created")
+        res.status(201).send(data);
     } catch (error) {
         res.status(500).send(error)
     }
     
+})
+//get course by id
+courseRoute.get('/course/:id',async(req,res) => {
+    try {
+        let cid  = req.params.id;
+        let course = await Course.findById(cid);
+        if(course != null){
+            res.status(200).send(course);
+        }else{
+            res.status(200).send("No Record Found");
+        }
+    } catch (error) {
+        res.status(404).send(error);
+    }
+})
+//Delete Course by id
+courseRoute.delete('/course/:id',async(req,res) => {
+    try {
+        let cid  = req.params.id;
+        let course = await Course.findByIdAndDelete(cid)
+        if(course != null){
+            res.status(200).send({"msg":"Record deleted"});
+        }else{
+            res.status(200).send({"msg":"No record found"});
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+//Update course by id
+courseRoute.patch('/course/:id',async(req,res) => {
+    try {
+        let cid  = req.params.id;
+        let course = await Course.findByIdAndUpdate(cid,req.body)
+        console.log(course)
+        if(course != null){
+            res.status(200).send({"msg":"Record updated"});
+        }else{
+            res.status(200).send({"msg":"No record found"});
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
+})
+//insert Multiple course
+courseRoute.post('/multicourse',async(req,res) => {
+    try {
+        let course = await Course.insertMany(req.body)
+        console.log(course)
+        if(course != null){
+            res.status(200).send(course);
+        }else{
+            res.status(200).send({"msg":"Not inserted"});
+        }
+    } catch (error) {
+        res.status(500).send(error);
+    }
 })
 module.exports = courseRoute;
