fa0d933366ca24ae25ac9c411c6e4e3669519e27
sfjbssept <sfjbssept@gmail.com>
2022-10-27 14:34:10 +0000

    added bootstrap and navbar component

--- angular-js/hello-world/src/app/app-routing.module.ts
+++ angular-js/hello-world/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { UserFormComponent } from './Components/user-form/user-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: UserFormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- angular-js/hello-world/src/app/app.module.ts
+++ angular-js/hello-world/src/app/app.module.ts
@@ -5,9 +5,11 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserFormComponent } from './Components/user-form/user-form.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { HomeComponent } from './Components/home/home.component';
 
 @NgModule({
-  declarations: [AppComponent, UserFormComponent],
+  declarations: [AppComponent, UserFormComponent, NavbarComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
