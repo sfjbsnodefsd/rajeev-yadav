76a393d17e3a8951e728422ee7f15220b85c3e0a
sfjbssept <sfjbssept@gmail.com>
2022-11-02 14:18:08 +0000

    Done Remove todo

--- angular-js/state-management-todo-list/src/app/components/display-todo/display-todo.component.ts
+++ angular-js/state-management-todo-list/src/app/components/display-todo/display-todo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Todo } from 'src/app/Entity/Todo';
 import { Store, select } from '@ngrx/store';
+import { TodoRemove } from 'src/app/actions/todo.action';
 @Component({
   selector: 'app-display-todo',
   templateUrl: './display-todo.component.html',
@@ -8,6 +9,9 @@ import { Store, select } from '@ngrx/store';
 })
 export class DisplayTodoComponent implements OnInit {
   todos: Todo[];
+  removeTodo(index: number) {
+    this.store.dispatch(new TodoRemove(index));
+  }
   constructor(private store: Store<{ todos: Todo[] }>) {
     store.pipe(select('todos')).subscribe((values) => {
       this.todos = values;
--- angular-js/state-management-todo-list/src/app/reducers/todo-reducer.ts
+++ angular-js/state-management-todo-list/src/app/reducers/todo-reducer.ts
@@ -1,14 +1,16 @@
 import { Todo } from '../Entity/Todo';
 import { ActionParent } from '../actions/todo.action';
 import { TodoActionType } from '../shared/enum/todo-action-types.enum';
-const initialState: Todo[] = [{ title: '' }];
+const initialState: Todo[] = [{ title: 'Test Todo' }];
 
 export function TodoReducer(state = initialState, action: ActionParent) {
   switch (action.type) {
     case TodoActionType.Add:
       return [...state, action.payload];
     case TodoActionType.Remove:
-      return [...state.splice(action.payload, 1)];
+      let newState = [...state];
+      newState.splice(action.payload, 1);
+      return newState;
     default:
       return state;
   }
